fef0fef0748dc97f18a26ad6e52266ed5764bfd2
Thiago van Dieten <thiago@tvdieten.com>
2025-04-18 15:57:40 +0200

    add command check and exit command

--- main.go
+++ main.go
@@ -8,11 +8,31 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) ([]string, error) {
 	if len(text) == 0 {
 		return []string{}, errors.New("input is empty")
 	}
 	return strings.Split(strings.ToLower(strings.TrimSpace(text)), " "), nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex...goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+var commands map[string]cliCommand = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the pokedex",
+		callback:    commandExit,
+	},
 }
 
 func main() {
@@ -27,5 +47,10 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("Your command was: %s\n", cleanedLine[0])
+
+		if c, ok := commands[cleanedLine[0]]; ok {
+			c.callback()
+		}
+
 	}
 }
