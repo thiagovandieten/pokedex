a8a03d3f576080c4b450cddaa622e4576961d4f0
Thiago van Dieten <thiago@tvdieten.com>
2025-04-22 09:17:24 +0200

    remove replace for commands package

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/thiagovandieten/pokedex
 
 go 1.24.2
-
-replace github.com/thiagovandieten/pokedex/commands => ./commands
--- main.go
+++ main.go
@@ -10,39 +10,12 @@ import (
 	"github.com/thiagovandieten/pokedex/commands"
 )
 
-// type cliCommand struct {
-// 	name        string
-// 	description string
-// 	callback    func() error
-// }
-
-// var currentCommands map[string]CliCommand
-
 func cleanInput(text string) ([]string, error) {
 	if len(text) == 0 {
 		return []string{}, errors.New("input is empty")
 	}
 	return strings.Split(strings.ToLower(strings.TrimSpace(text)), " "), nil
 }
-
-// func commandExit() error {
-// 	fmt.Println("Closing the Pokedex... Goodbye!")
-// 	os.Exit(0)
-// 	return nil
-// }
-
-// func commandMap() error {
-// 	return nil
-// }
-
-// func commandHelp() error {
-// 	fmt.Println("Welcome to the Pokedex!")
-// 	fmt.Println("Usage:")
-// 	for _, cmd := range commands {
-// 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-// 	}
-// 	return nil
-// }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
